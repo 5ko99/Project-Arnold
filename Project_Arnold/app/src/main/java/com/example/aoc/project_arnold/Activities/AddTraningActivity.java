cadd1ff6e0f2c163478b291243c363cf683cc91e
AOC <petko.krasimirov99@gmail.com>
2016-05-27 18:25:04 +0300

    Activity work and database design
    1.Added database design on page 4 on Project Arnold Notebook
    2.Created design and some java code on AddTraningActivity

--- Project_Arnold/app/src/main/java/com/example/aoc/project_arnold/Activities/AddTraningActivity.java
+++ Project_Arnold/app/src/main/java/com/example/aoc/project_arnold/Activities/AddTraningActivity.java
@@ -1,15 +1,43 @@
 package com.example.aoc.project_arnold.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 
 import com.example.aoc.project_arnold.R;
 
-public class AddTraningActivity extends AppCompatActivity {
-
+public class AddTraningActivity  extends Activity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
+    DatePicker dp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_traning);
+
+        dp=(DatePicker) findViewById(R.id.add_training_date_picker);
+        spinner = (Spinner) findViewById(R.id.add_training_spinner_training_types);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.traning_type, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
