b9836fb8d8510ad47284d52d0cbee73302961f78
AOC <petko.krasimirov99@gmail.com>
2016-06-06 11:12:46 +0300

    Code about info
    Get all info from db abbout traning and dispalyed it in
    activity_traning_info.xml

--- Project_Arnold/app/src/main/java/com/example/aoc/project_arnold/Activities/TraningInfoActivity.java
+++ Project_Arnold/app/src/main/java/com/example/aoc/project_arnold/Activities/TraningInfoActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,15 +21,19 @@ public class TraningInfoActivity extends AppCompatActivity {
     private String type;
     private String date;
     private String recordExercise;
-    private  int  recordKg;
+    private int recordKg;
     private int recordReps;
 
     //View with info about training
     private TextView tv_type;
     private TextView tv_date;
+    private TextView tv_record;
     private TextView tv_recordExercise;
     private TextView tv_recordKg;
     private TextView tv_recordReps;
+    private TextView tv_recordExerciseTittle;
+    private TextView tv_recordKgTittle;
+    private TextView tv_recordRepsTittle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,36 +47,58 @@ public class TraningInfoActivity extends AppCompatActivity {
         date = bundle.getString("date");
 
         dbPref = new DBPref(this);
-        mCursor = dbPref.getVals(type,date);
+        mCursor = dbPref.getVals(type, date);
 
         //TODO: Fix error with reading form db
-        if (mCursor.moveToFirst()){
-            do{
-                recordExercise=mCursor.getString(mCursor.getColumnIndexOrThrow("record_exercise"));
-                recordKg=mCursor.getInt(mCursor.getColumnIndex("record_kg"));
-                recordReps=mCursor.getInt(mCursor.getColumnIndex("record_reps"));
-            }while(mCursor.moveToNext());
+        if (mCursor.moveToFirst()) {
+            do {
+                recordExercise = mCursor.getString(mCursor.getColumnIndexOrThrow("record_exercise"));
+                recordKg = mCursor.getInt(mCursor.getColumnIndex("record_kg"));
+                recordReps = mCursor.getInt(mCursor.getColumnIndex("record_reps"));
+            } while (mCursor.moveToNext());
         }
 
 
         mCursor.close();
         dbPref.close();
 
+        setInfoFromDB();
 
-        StringBuilder info = new StringBuilder();
-        info.append(position + " " + type + " " + date + " " + recordExercise + "  " + recordKg+ "  "+ recordReps);
+    }
 
-        Toast.makeText(this,info.toString(), Toast.LENGTH_LONG).show();
-
-        //TODO: Show info in TextView
+    private void setInfoFromDB() {
+        //Extract in method
 
         //Initialise all tv
-        tv_type= (TextView) findViewById(R.id.tv_info_type);
+        tv_type = (TextView) findViewById(R.id.tv_info_type);
         tv_date = (TextView) findViewById(R.id.tv_info_date);
-
+        tv_record = (TextView) findViewById(R.id.tv_info_record);
+        tv_recordExercise = (TextView) findViewById(R.id.tv_info_record_exercise);
+        tv_recordKg = (TextView) findViewById(R.id.tv_info_record_kg);
+        tv_recordReps = (TextView) findViewById(R.id.tv_info_record_reps);
+        tv_recordExerciseTittle = (TextView) findViewById(R.id.tv_info_record_exercise_tittle);
+        tv_recordKgTittle = (TextView) findViewById(R.id.tv_info_record_kg_title);
+        tv_recordRepsTittle = (TextView) findViewById(R.id.tv_info_record_reps_tittle);
         //Set info to tv
         tv_type.setText(type);
         tv_date.setText(date);
 
+        //Check if this traning have record
+        if (recordExercise.matches("No record")) {
+            //If there is no record hide view
+            tv_record.setVisibility(View.GONE);
+            tv_recordExercise.setVisibility(View.GONE);
+            tv_recordKg.setVisibility(View.GONE);
+            tv_recordReps.setVisibility(View.GONE);
+            tv_recordExerciseTittle.setVisibility(View.GONE);
+            tv_recordKgTittle.setVisibility(View.GONE);
+            tv_recordRepsTittle.setVisibility(View.GONE);
+        } else {
+            //If is record show info in views
+            tv_recordExercise.setText(recordExercise);
+            tv_recordKg.setText(Integer.toString(recordKg));
+            tv_recordReps.setText(Integer.toString(recordReps));
+        }
     }
+
 }
